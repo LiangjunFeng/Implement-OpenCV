10b80c6ace5ca983d21f50dc7fed93eb7000d1bd
Mr.  Little Zhu <zhumavip@163.com>
2017-10-30 06:36:14 -0500

    Update 8.ICAFilter.py

--- 8.ICAFilter.py
+++ 8.ICAFilter.py
@@ -1,90 +1,47 @@
+import wave
+import os
 import numpy as np
-import cv2 as cv
-from sklearn import preprocessing
-from numpy import random
+from sklearn.decomposition import FastICA
 import math
+import matplotlib.pyplot as plt
+from numpy import random
+from sklearn import preprocessing 
 
-def LoadPicture(path):                   #Load the picture's data ,and reshape it
-    img = cv.imread(path,0)
-    img = cv.resize(img,(300,300))
-    return img
+def LoadSound(path):
+    f = wave.open(path,'rb')
+    params = f.getparams()
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    strData = f.readframes(nframes)               #read the wav file
+    waveData = np.fromstring(strData,dtype=np.int16) 
+    waveData = waveData*1.0/(max(abs(waveData)))  #normalize the sound wave
+    time = np.arange(0,nframes*nchannels)*(1.0 / framerate)
+    return time.tolist(),waveData.tolist() 
 
-def addSaltyNoise(image,count = 1000):                 #add salty noise on the picture , and meanwhile  
-    img = image.copy()                                 #create another two pictues,one for ica,one for comparing
-    img2 = np.zeros((img.shape[0],img.shape[1]))
-    img3 = image.copy()
-    coutn = count
-    for k in range(coutn):
-        xi = int(np.random.uniform(0,img.shape[1]))
-        xj = int(np.random.uniform(0,img.shape[0]))
-        if img.ndim == 2:
-            img[xj,xi] = 255
-            img2[xj,xi] = 255
-            img3[xj,xi] = 0
-        elif img.ndim == 3:
-            img[xj,xi,0] = 255
-            img[xj,xi,1] = 255
-            img[xj,xi,2] = 255
-    return img,img2,img3
+def plotSound(time,data):                         #plot the sound 
+    plt.figure()
+    plt.plot(time,data)
+    plt.xlabel("Time(s)")
+    plt.ylabel("Amplitude")
+    plt.title("Single channel wavedata")
+    plt.grid('on')
 
-def show(name,img):                       #show a picture
-    cv.namedWindow(name)
-    cv.imshow(name,img)
-    cv.waitKey()
-
-def close():                             #close all the pictures
-    cv.destroyAllWindows()
-    cv.waitKey(1)
-    
-def normalize(image):                    #normalize the picture that have been processed to unit8 type
-    img = image.copy()
-    img -= img.min()
-    img /= img.max()
-    img = img*255
-    img = img.astype(np.uint8)
-    return img
-
-def mFilter(image):                      #show the result of some other filters
-    img = image.copy()
-    kernel = np.ones((5,5),np.float32)/25
-    c = cv.filter2D(img,-1,kernel)
-    d = cv.blur(img,(3,5))
-    e = cv.bilateralFilter(img,9,75,75)
-    f = cv.GaussianBlur(img,(5,5),0)
-    show('org',img)
-    show('box',c)
-    show('blur',d)
-    show('bilateral',e)
-    show('Gauss',f)
-    
-def showICA(dataS,length,bordth):         #show the results of ica
-    a = dataS[:,0].reshape(length,bordth)
-    b = dataS[:,1].reshape(length,bordth)
-    a = normalize(a)
-    b = normalize(b)
-    c = a-b
-    show('isolateb',a)
-    show('isolatea',b)
-    show('minus',c)
-    
-def transferToMatrix(image1,image2,length,bordth):      #make matrix from two pictures
-    data = []    
-    a = image1.reshape(length*bordth,1)
-    a = (np.ravel(a)).tolist()
-    data.append(a)
-    
-    b = image2.reshape(length*bordth,1)
-    b = (np.ravel(b)).tolist()
-    data.append(b)  
-    
-    data = np.mat(data)  
-    return data
-
-def getRandomW(length):                                 #creat a square matrix
-    W = random.random(size=(length,length))
+def getRandomW(length,height):                    #make a random matrix
+    W = random.random(size=(length,height))
     return W
-
-def GFunction(data):                                    #the first derivate function
+    
+def eigOrth(Data):                                #eigenormalize the data
+    data = Data.copy()
+    D,E = np.linalg.eig(data.dot(data.T))
+    for i in range(len(D)):
+        if D[i] < 1e-7:
+            D[i] = 0.01
+        D[i] = D[i]**0.5
+    D = np.mat(np.diag(D))
+    D = D.I
+    data = E*D*E.T*data
+    return data.real
+    
+def GFunction(data):                              #the first derivate function in ICA                        
     def G(x):
         y = x*math.exp(-0.5*(x**2))
         return y
@@ -95,7 +52,7 @@ def GFunction(data):                                    #the first derivate func
             output[i,j] = G(data[i,j])
     return output
 
-def gFunction(data):                                  #the second derivate function
+def gFunction(data):                             #the second derivate function in ICA    
     def g(x):
         y = -1*(x**2)*math.exp(-0.5*(x**2))
         return y
@@ -104,60 +61,97 @@ def gFunction(data):                                  #the second derivate funct
     for i in range(length):
         for j in range(bordth):
             output[i,j] = g(data[i,j])
-    return output   
+    return output 
 
-def distance(W,oldW):                                 #compare the differences between two pictures
+def distance(W,oldW):                            #using at judging convergence
     length = len(W)
-    t = 0.
+    s = 0.
     for i in range(length):
-        t += abs(float(W[i]) - float(oldW[i]))
-    return t  
+        s += abs(float(W[i]) - float(oldW[i]))
+    return s   
 
-def eigOrth(data):                                    #orthogonalization
-    data = np.mat(preprocessing.scale(data.T).T)
-    D,E = np.linalg.eig(data.dot(data.T))
-    D = np.diag(D**(-0.5))
-    data = E*D*E.T*data
-    return data
+class ICA:                                      #ICA
+    def __init__(self,conponent = -1):
+        self._W = []
+        self._conponent = conponent
+        
+    def fit_transform(self,data):
+        data = preprocessing.scale(data.T)
+        data = np.mat(eigOrth(data.T))
+        if self._conponent == -1:
+            self._conponent = data.shape[0]
+        W = getRandomW(data.shape[0],self._conponent)
+        W = eigOrth(W)
+        MAX_T = 10000
+        
+        for i in range(W.shape[1]):
+            w = W[:,i]
+            j,t  = 0,1
+            while (j < MAX_T) and (t > 1e-7):
+                oldw = w.copy()
+                w -= (np.mean(np.multiply(data,GFunction(w.T*data)),1) - np.mean(w.T*data)*w)
+                temp = np.zeros((W.shape[0],1))
+                for k in range(i):
+                    temp += float(w.T*W[:,k])*W[:,k]
+                w = w - temp
+                w = w/math.sqrt(w.T*w)
+                W[:,i] = w
+                t = distance(W[:,i],oldw)
+                print(i+1,t)
+                j += 1
+        self._W = W
+        return self._W.T*data
+    
+    def transfer(self,data):
+        data = preprocessing.scale(data.T)
+        data = np.mat(eigOrth(data.T))
+        return self._W.T*data
+
 
 if __name__ == '__main__':
-#load the picture,convert it to n*2's matrix,init W
-#====================================================================
-    path = "/Users/zhuxiaoxiansheng/Desktop/Lena.png"
-    image = LoadPicture(path)
-    length,bordth = image.shape
-    imageSalty,virtualSalty,comp = addSaltyNoise(image,5000)
-    data = transferToMatrix(imageSalty,virtualSalty,length,bordth)
-    data = eigOrth(data)
-    W = getRandomW(data.shape[0])
-    W = eigOrth(W)
+#========================================================================
+#Load the data and make them the same size
+    file1 = "/Users/zhuxiaoxiansheng/Desktop/LDC2017S07.clean.wav" 
+    file2 = "/Users/zhuxiaoxiansheng/Desktop/LDC2017S10.embed.wav"
+    file3 = "/Users/zhuxiaoxiansheng/Desktop/LDC93S1.wav"
+    time1,data1 = LoadSound(file1)
+    time2,data2 = LoadSound(file2)
+    time3,data3 = LoadSound(file3)
+  
+    time1 = time1[1000:-1000]
+    data1 = data1[1000:-1000]
+    time2 = time1
+    time3 = time1
+    data2 = data2[0:len(time1)]
+    data3 = data3[3000:3000+len(time1)]
 
-
-#Figure out W using Newton's method
-#====================================================================
-    MAX_T = 10000
-    for i in range(W.shape[0]):
-        j,t,w = 0,1,W[:,i]
-        while j < MAX_T and t > 5e-14:
-            oldw = w.copy()
-            for k in range(W.shape[1]):
-                w[k] = np.mean(np.multiply(data[k,:],GFunction(w.T*data)))-np.mean(np.multiply(gFunction(w.T*data),w[k]))
-            temp = np.zeros((W.shape[0],1))
-            for k in range(i):
-                temp += float(w.T*W[:,k])*W[:,k]
-            w = w - temp
-            w = w/math.sqrt(w.T*w)
-            W[:,i] = w
-            t = distance(W[:,i],oldw)
-            print(t)
-            j += 1
-    dataS = (W.T.dot(data)).T
+#=======================================================================
+#add the three sounds between each other ,create three mix sounds 
+    dataMerage1 = np.vstack((np.mat(data1),np.mat(data2)))
+    dataMerage1 = (np.ravel(dataMerage1.sum(axis = 0))).tolist()  
+    dataMerage2 = np.vstack((np.mat(data3),np.mat(data2)))
+    dataMerage2 = (np.ravel(dataMerage2.sum(axis = 0))).tolist()
+    dataMerage3 = np.vstack((np.mat(data1),np.mat(data3)))
+    dataMerage3 = (np.ravel(dataMerage3.sum(axis = 0))).tolist()
     
-#compare the reslut with other method  
-#====================================================================
-    show('org',image)
-    show('salty',imageSalty)
-    show('compare',comp)
-    showICA(dataS,length,bordth)   
-    mFilter(imageSalty)
-    close()
+    plotSound(time1,data1)
+    plotSound(time2,data2)
+    plotSound(time3,data3)      #plot three mix sounds
+
+#=======================================================================
+    data = np.vstack((np.mat(dataMerage1),np.mat(dataMerage2)))
+    data = np.vstack((data,np.mat(dataMerage3)))
+  
+    sica = SICA()
+    dataS = sica.fit_transform(data.T).T
+
+    dataS1 = dataS[0]
+    dataS1 = np.ravel(dataS1).tolist()
+    dataS2 = dataS[1]
+    dataS2 = np.ravel(dataS2).tolist()
+    dataS3 = dataS[2]
+    dataS3 = np.ravel(dataS3).tolist()
+
+    plotSound(time1,dataS1)
+    plotSound(time2,dataS2)
+    plotSound(time3,dataS3)     #plot sounds of unmixing by ica
